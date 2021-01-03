2efbf798f656f457a4ca9e7010bd8fac3ee2c765
zeinebzarrouk <63512721+zeinebzarrouk@users.noreply.github.com>
2021-01-03 20:34:37 +0100

    Update regions.php

--- FRONT/Models/regions.php
+++ FRONT/Models/regions.php
@@ -7,20 +7,15 @@ include_once '../Controller/regionsc.php';
 class region{
     private $idR;
     private $nom;
-    private $DateDebut;
-    private $DateFin;
     private $photo;
-    private $information;
-    private $cout;
+    private $description;
+  
 
- function __construct($IdR,$NomRegion,$Photo,$information,$dateDebut,$dateFin,$Cout){
+ function __construct($IdR,$NomRegion,$Photo,$description){
     $this->id=$IdR;
     $this->nom=$NomRegion;
     $this->photo=$Photo; 
-    $this->information=$information;
-    $this->DateDebut=$dateDebut;
-    $this->DateFin=$dateFin;
-    $this->cout=$Cout; 
+    $this->description=$description;
  }
 
  function getid(){
@@ -30,23 +25,12 @@ class region{
     return $this->nomRegion;
  }
  
-
  function getphoto(){
     return $this->photo;
  }
  function getinformation(){
-   return $this->information;
+   return $this->description;
 }
-function getDateDebut(){
-   return $this->DateDebut;
-}
-function getDateFin(){
-   return $this->DateFin;
-}
-function getCout(){
-   return $this->cout;
-}
-
 
 
  function setid($IdR){
@@ -60,17 +44,10 @@ function getCout(){
      $this->photo=$Photo;
  }
  function setinformation($information){
-   $this->information=$information;
+   $this->description=$description;
 }
-function setDateDebut($dateDebut){
-   $this->DateDebut=$dateDebut;
-}
-function setDateFin($dateFin){
-   $this->DateFin=$dateFin;
-}
-function setCout($Cout){
-   $this->cout=$Cout;
-}
+
+
 
 
 
