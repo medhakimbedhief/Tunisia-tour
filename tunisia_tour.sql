8bcaa1ce3b52f527a5f1eff873d25fa75976e555
zeinebzarrouk <63512721+zeinebzarrouk@users.noreply.github.com>
2021-01-03 20:31:02 +0100

    Update tunisia_tour.sql

--- tunisia_tour.sql
+++ tunisia_tour.sql
@@ -183,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `region` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL, 
   `photo` varchar(255) NOT NULL,
-  `information` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
@@ -191,7 +191,7 @@ CREATE TABLE IF NOT EXISTS `region` (
 -- Déchargement des données de la table `region`
 --
 
-INSERT INTO `region` (`id`, `nom`, `photo`, `information`, ) VALUES
+INSERT INTO `region` (`id`, `nom`, `photo`, `description`, ) VALUES
 (27, 'nabeul', 'tÃ©lÃ©chargement.jpg', 'ttt');
 
 -- --------------------------------------------------------
@@ -233,6 +233,7 @@ ALTER TABLE `panier`
   ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`iduser`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `panier_ibfk_2` FOREIGN KEY (`idchoix`) REFERENCES `camping` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `panier_ibfk_3` FOREIGN KEY (`idchoix`) REFERENCES `randonnee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `panier_ibfk_4` FOREIGN KEY (`idchoix`) REFERENCES `region` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
